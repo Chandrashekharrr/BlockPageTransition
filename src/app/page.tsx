a6316ecdd98e1e10f5390a9d4512aa4a1b6b4af7
Chandrashekhar <chandrashekharcorp@gmail.com>
2026-07-03 13:23:32 +0530

    transition working fine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to the Home Page</h1>
-    </main>
-  );
+const Genesis = () => {
+  return(
+    <section className="hero genesis">
+      <h1>Genesis</h1>
+    </section>
+  )
 }
+
+export default Genesis;
